a8b4f1a41d0448091d63fb22985ee57369f5cc33
Amartya Mishra <kushmishra8@gmail.com>
2020-05-04 13:25:41 +0530

    Express sessions pt.1

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+var session = require('express-session');
+var Filestore = require('session-file-store')(session);
 
-const Dishes = require('./models/dishes')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter')
@@ -30,16 +31,23 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-98765-43212'));
+//app.use(cookieParser('12345-67890-98765-43212'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-98765-43212',
+  saveUninitialized: false,
+  resave: false,
+  store: new Filestore()
+}));
 
 function auth (req, res, next) {
-  console.log(req.signedCookies);
-  if(!req.signedCookies.user){
+  console.log(req.session);
+  if(!req.session.user){
     var authHeader = req.headers.authorization;
 
     if (!authHeader) {
       var err = new Error('You are not authenticated');
-      res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
       next(err);
       return;
@@ -49,7 +57,7 @@ function auth (req, res, next) {
     var user = auth[0];
     var pass = auth[1];
     if(user == 'admin' && pass == 'password'){
-      res.cookie('user', 'admin', {signed:true});
+      req.session.user = 'admin';
       next();   //authorized
     } 
     else {
@@ -61,12 +69,11 @@ function auth (req, res, next) {
     }
   }
   else{
-    if(req.signedCookies.user === 'admin'){
+    if(req.session.user === 'admin'){
       next();
     }
     else{
-      var err = new Error('You are not authenticated');
-      res.setHeader('WWW-Authenticate', 'Basic');
+      var err = new Error('You are not authenticated');s
       err.status = 401;
       next(err);
       return;
